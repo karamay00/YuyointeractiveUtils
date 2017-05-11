bb3b680e07026c188f3e68d06246f76d3f678b9e
karamay00 <panrui406@163.com>
2017-05-11 14:01:06 +0800

    解决eclipse下desktop的资源路径问题

--- core/src/com/yuyointeractive/utils/MyAssetUtil.java
+++ core/src/com/yuyointeractive/utils/MyAssetUtil.java
@@ -2,6 +2,7 @@ package com.yuyointeractive.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
@@ -14,8 +15,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class MyAssetUtil {
   public static String getAssetsPath(String assetsPath) {
-    return Gdx.app.getType() == ApplicationType.Desktop ? "./bin/" + assetsPath + "/" : assetsPath + "/";
-    //return assetsPath + "/";
+    if (Gdx.app.getType() == ApplicationType.Desktop && Gdx.files.getFileHandle("bin", FileType.Internal).exists()) {
+      return "./bin/" + assetsPath + "/";
+    } else {
+      return assetsPath + "/";
+    }
   }
   public static void loadAssets(AssetManager assetManager, String assetsPath) {
     assetsPath = getAssetsPath(assetsPath);
