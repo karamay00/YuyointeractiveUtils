f3fbf7fcf973cc7ee2663530e8c8a5282472dba4
karamay00 <panrui406@163.com>
2019-09-08 16:32:59 +0800

    去掉MyCrossPlatformHandler所有方法，然后再在每个项目中再具体写

--- core/src/com/yuyointeractive/utils/MyCrossPlatformHandler.java
+++ core/src/com/yuyointeractive/utils/MyCrossPlatformHandler.java
@@ -25,11 +25,11 @@ public interface MyCrossPlatformHandler {
   //
  // public void quit();
   // public void login(Gamer gamer, OnLoginListener listener);
-  public void loginWeChat(MyNetworkListener listener);
+//  public void loginWeChat(MyNetworkListener listener);
   //public void loginAlipay();
-  public void shareWeChatTimeLine(String title, String description);
-  public void shareWeChatSession(String title, String description);
-  public void shareWeChatImage();
+//  public void shareWeChatTimeLine(String title, String description);
+//  public void shareWeChatSession(String title, String description);
+//  public void shareWeChatImage();
   //public void shareAlipay(String str);
   
  // public void startSpeak();
@@ -38,12 +38,12 @@ public interface MyCrossPlatformHandler {
  // public void joinChannel(int roomId, int myId);
  // public void leaveChannel();
 
-public void startRecordVoice();
-public void stopRecordVoice(boolean isCancelUpload);
-public void playVoice(String url,byte pos);
-public void loginYunvaIm(String id, String nickname);
+//public void startRecordVoice();
+//public void stopRecordVoice(boolean isCancelUpload);
+//public void playVoice(String url,byte pos);
+//public void loginYunvaIm(String id, String nickname);
 
-public void universal(Object object);
+//public void universal(Object object);
   
   
 //  public void startRecordVoice(String id);
